f15b1682d240de63016e8be9c16c7d4aa2e7bc3c
Jordan Hillenburg <31252097+jhillenburg@users.noreply.github.com>
2020-11-06 20:08:50 -0600

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -10,7 +10,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
+    if (username === "user" && password === "test") {
         // If the credentials are valid, show an alert box and reload the page
         alert("You have successfully logged in.");
         location.reload();
